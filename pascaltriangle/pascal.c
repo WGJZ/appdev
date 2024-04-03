9bbcabdbd7f2b92e221a2b5d0d0b3f1d64ed821c
WGJZ <151679407+WGJZ@users.noreply.github.com>
2024-04-03 15:50:27 +0300

    Update pascal.c with 2 lines comments

--- pascaltriangle/pascal.c
+++ pascaltriangle/pascal.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include "screen.h"
+//function defination of making the triangle
 void pascal_triangle(int rows) {
 	int i, k;
 
@@ -12,6 +13,8 @@ void pascal_triangle(int rows) {
 		printf("\n");
 	}
 }
+
+//function defination of n select k
 int binomial(int n, int k) {
 	return factorial(n) / factorial(k) / factorial(n - k);
 
